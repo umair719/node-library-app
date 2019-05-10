870a2984ac0d7b785c4210b74a4405adab517293
Khalid, Umair <Umair.Khalid@McKesson.com>
2019-05-10 15:28:25 -0700

    added eslint config

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-var express = require('express');
-var chalk = require('chalk');
-var debug = require('debug')('app');
-var morgan = require('morgan');
-var path = require('path');
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express();
+const app = express();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 
-app.get('/', function (req, res) {
-    // res.send('Hello from my library app!');
-    res.sendFile(path.join(__dirname, 'views/index.html'));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+
+
+app.get('/', (req, res) => {
+  // res.send('Hello from my library app!');
+  res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.listen(3000, function () {
-    debug(`listening on port ${chalk.green('3000')}`);
+app.listen(3000, () => {
+  debug(`listening on port ${chalk.green('3000')}`);
 });
